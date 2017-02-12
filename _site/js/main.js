b76d04f756ccc318d37eeeeb96636cb0ea38a158
enistico <ericastacey@Ericas-MacBook-Air.local>
2017-02-12 14:25:39 +0800

    Day 9 (second commit) - finalising html content and css styling, addition of Event Calendar
    replicating course page content layout on all course pages, setting up
    homepage and default layouts, adding Event Calendar, troubleshooting
    grid style conflicts

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,5 +1,55 @@
 $(document).ready( function () {
 
+// Full Calendar
+$('#calendar').fullCalendar({
+        // put your options and callbacks here
+
+        eventSources: [
+
+        // your event source
+        {
+            events: [ // put the array in the `events` property
+                {
+                    title  : 'Introduction to Google Analytics',
+                    start  : '2017-03-14',
+                    url    : '/google-analytics-intro.html',
+                },
+                {
+                    title  : 'Taking Google Analytics Further',
+                    start  : '2017-03-28',
+                    url    : '/google-analytics-further.html',
+                },
+                {
+                    title  : 'Introduction to SEO Best Practices',
+                    start  : '2017-04-11',
+                    url    : '/seo-basics.html',
+                },
+                {
+                    title  : 'Introduction to Google Analytics',
+                    start  : '2017-04-25',
+                    url    : '/google-analytics-intro.html',
+                },
+                {
+                    title  : 'Taking Google Analytics Further',
+                    start  : '2017-05-09',
+                    url    : '/google-analytics-further.html',
+                },
+                {
+                    title  : 'Introduction to SEO Best Practices',
+                    start  : '2017-05-23',
+                    url    : '/seo-basics.html',
+                }
+            ],
+            color: 'orange',     // an option!
+            textColor: 'black' // an option!
+        }
+
+        // any other event sources...
+
+    ]
+
+    })
+
 // Owl Carousel
 // basic demo, responsive with next/prev nav
 $('.owl-carousel').owlCarousel({
